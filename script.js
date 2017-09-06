07d92902747c41b9898c9a9836369af4c360c626
andrewfair <afair@andrews-mbp.pratt.edu>
2017-09-05 21:22:18 -0400

    add files

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 var myArray = ["pottery", "billiards", "running"];
-myArray.forEach(function(x){console.log(x);});
+myArray.forEach(function(x){console.log("I like", x);});
+
+var otherArray = {fav1: "chocolate", fav2: "quality sleep", fav3: "making stuff"};
+
+for (var key in otherArray) {
+	console.log(key, otherArray[key]);
+}
